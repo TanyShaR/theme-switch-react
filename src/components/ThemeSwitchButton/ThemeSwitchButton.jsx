823089dca2f06df8423524623016d6e475bcc4cc
TanjaRetter <88773218+TanyShaR@users.noreply.github.com>
2025-07-04 15:00:41 +0200

    adjustment

--- src/components/ThemeSwitchButton/ThemeSwitchButton.jsx
+++ src/components/ThemeSwitchButton/ThemeSwitchButton.jsx
@@ -1,6 +1,7 @@
 import classNames from "classnames";
 import S from "./ThemeSwitchButton.module.css";
 
+
 export const ThemeSwitchButton = ({ value, active, onSwitch, children }) => {
   /* const className = active ? `${S.container} ${S.active}` : S.container; */
   const className = classNames(S.container, active && S.active);
